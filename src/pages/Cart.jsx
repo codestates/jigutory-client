5f2df3b367115823c2e0d23bd0a95f8abcb3c9a4
heesue <78947950+kang-heesue@users.noreply.github.com>
2021-07-14 17:40:58 +0900

    fix: merge error (#78)
    * feat: mypage
    
    * fix: error (#56)
    
    * fix: token error
    
    * ignore
    
    * feat : done but profile image & fix : placeholder on input
    
    * feat: badge
    
    * feat : need to fix alert message
    
    * feat : show level info & no error on console
    
    * fix : handleClickNum event
    
    * feat: badge complete (#62)
    
    * fix: error (#56)
    
    * fix: token json (#57)
    
    * feat: mypage
    
    * fix: token error
    
    Co-authored-by: Hyunsoo <72371860+Hyunsoosiesta@users.noreply.github.com>
    
    * ignore
    
    * feat : done but profile image & fix : placeholder on input
    
    * feat : need to fix alert message
    
    * feat: badge complete
    
    Co-authored-by: Hyunsoo <72371860+Hyunsoosiesta@users.noreply.github.com>
    Co-authored-by: Hyunsoo Kang <hyunsoosiesta@gmail.com>
    
    * feat : need to take level info
    
    * fix: merge error (#64)
    
    * style : work on css & structure
    
    * fix : work on level but 1)not working clickNum 2)imageurl
    
    * feat : work on updated server, cannot get data from db for now
    
    * save : before chart css
    
    * docs : add loading component
    
    * feat: map overlay (#65)
    
    * test
    
    * style : css done excluding modal & RWD
    
    * feat: overlay(working on)
    
    * stype : css done excluding modal & RWD, test levelinfo & clickNum
    
    * feature/for deploy
    
    * fix : it works if clickNum is over 1
    
    * fix : mypage level functional issue (excluding levelinfo when refreshing page  and badge css)
    
    * style : css done
    
    * feat: scrollbutton complete (#68)
    
    * feat: overlay2
    
    * feat: scrollbutton
    
    * fix: error (#73)
    
    * feat: overlay2
    
    * feat: scrollbutton
    
    * fix: overlay3
    
    * style : css done including badge
    
    * style: merge mypage (#74)
    
    * style : css done excluding modal & RWD
    
    * stype : css done excluding modal & RWD, test levelinfo & clickNum
    
    * fix : it works if clickNum is over 1
    
    * fix : mypage level functional issue (excluding levelinfo when refreshing page  and badge css)
    
    * style : css done
    
    * style : css done including badge
    
    Co-authored-by: Hyunsoo Kang <hyunsoosiesta@gmail.com>
    
    * fix: pull css (#75)
    
    * feature / market & cart
    
    * feat: overlay2
    
    * feat: scrollbutton
    
    * fix: overlay3
    
    * style: merge mypage (#74)
    
    * style : css done excluding modal & RWD
    
    * stype : css done excluding modal & RWD, test levelinfo & clickNum
    
    * fix : it works if clickNum is over 1
    
    * fix : mypage level functional issue (excluding levelinfo when refreshing page  and badge css)
    
    * style : css done
    
    * style : css done including badge
    
    Co-authored-by: Hyunsoo Kang <hyunsoosiesta@gmail.com>
    
    Co-authored-by: i2sign <i2sign@icloud.com>
    Co-authored-by: Hyunsoo Kang <hyunsoosiesta@gmail.com>
    
    * feat : add loading spinner when update clickNum
    
    * feature/market
    
    * feature/cart
    
    * style : build RWD working on badge part
    
    * style : done
    
    * style : done addcart css
    
    * feature/adjust_cart
    
    * style : done addcart modal css
    
    * feat: overlay click&mouse (#76)
    
    * feature / market & cart
    
    * feat: overlay2
    
    * feat: scrollbutton
    
    * fix: overlay3
    
    * style: merge mypage (#74)
    
    * style : css done excluding modal & RWD
    
    * stype : css done excluding modal & RWD, test levelinfo & clickNum
    
    * fix : it works if clickNum is over 1
    
    * fix : mypage level functional issue (excluding levelinfo when refreshing page  and badge css)
    
    * style : css done
    
    * style : css done including badge
    
    Co-authored-by: Hyunsoo Kang <hyunsoosiesta@gmail.com>
    
    * feat: overlay click&mouseover
    
    Co-authored-by: i2sign <i2sign@icloud.com>
    Co-authored-by: Hyunsoo Kang <hyunsoosiesta@gmail.com>
    
    Co-authored-by: Hyunsoo <72371860+Hyunsoosiesta@users.noreply.github.com>
    Co-authored-by: Hyunsoo Kang <hyunsoosiesta@gmail.com>
    Co-authored-by: i2sign <i2sign@icloud.com>

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,102 +2,109 @@ import React, { useEffect, useState, useCallback } from 'react';
 import { useHistory, withRouter } from 'react-router-dom';
 import axios from 'axios';
 import '../styles/CartList.scss';
-import CartList from '../components/CartList'
+import CartList from '../components/CartList';
 import OrderSummary from '../components/OrderSummary';
 
 function Cart(accessToken, userInfo, handleLogin) {
   const history = useHistory();
-  const [cartList, setCartList] = useState([])
-  const [userinfo, setuserInfo] = useState('')
-  const [checkedItems, setCheckedItems] = useState(cartList.map((el) => el.itemId))
-  const [total, setTotal] = useState([])
+  const [cartList, setCartList] = useState([]);
+  const [userinfo, setuserInfo] = useState('');
+  const [checkedItems, setCheckedItems] = useState(
+    cartList.map((el) => el.itemId),
+  );
+  const [total, setTotal] = useState([]);
 
   useEffect(() => {
-    const dataLocalStorage = localStorage.getItem('userInfo')
-    if(dataLocalStorage){
-      setuserInfo(JSON.parse(dataLocalStorage))
+    const dataLocalStorage = localStorage.getItem('userInfo');
+    if (dataLocalStorage) {
+      setuserInfo(JSON.parse(dataLocalStorage));
     }
-  },[setuserInfo])
+  }, [setuserInfo]);
 
-  useEffect(()=>{
-    if(userinfo.email){
-      axios.post('http://localhost:4000/cart/read',{
-        email:userinfo.email
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json',
-        },
+  useEffect(() => {
+    if (userinfo.email) {
+      axios
+        .post(
+          'http://localhost:4000/cart/read',
+          {
+            email: userinfo.email,
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          },
+        )
+        .then((res) => {
+          setCartList(res.data);
+        });
     }
-  ).then((res)=>{
-    setCartList(res.data)
-  })
-}})
+  });
 
-// const handleCheckChange = (checked, id) => {
-//   if (checked) {
-//     setCheckedItems([{...checkedItems, id}]);
-//   }
-//   else {
-//     setCheckedItems(checkedItems.filter((el) => el !== id));
-//   }
-// };
+  // const handleCheckChange = (checked, id) => {
+  //   if (checked) {
+  //     setCheckedItems([{...checkedItems, id}]);
+  //   }
+  //   else {
+  //     setCheckedItems(checkedItems.filter((el) => el !== id));
+  //   }
+  // };
 
-// const handleAllCheck = (checked) => {
-//   if (checked) {
-//     setCheckedItems(cartList.map((el) => el.itemId))
-//   }
-//   else {
-//     setCheckedItems([]);
-//   }
-// };
-//   const sum = {}
-//   const onQuantityChange = (itemId, quantity) => {
-//     console.log(`thisiscartList`, cartList)
-//     const found = cartList.filter((el) => el.id == itemId)
-//     const idx = cartList.indexOf(found)
-//     console.log(sum)
-//     const cartItem = {
-//       itemId,
-//       quantity
-//     }
+  // const handleAllCheck = (checked) => {
+  //   if (checked) {
+  //     setCheckedItems(cartList.map((el) => el.itemId))
+  //   }
+  //   else {
+  //     setCheckedItems([]);
+  //   }
+  // };
+  //   const sum = {}
+  //   const onQuantityChange = (itemId, quantity) => {
+  //     console.log(`thisiscartList`, cartList)
+  //     const found = cartList.filter((el) => el.id == itemId)
+  //     const idx = cartList.indexOf(found)
+  //     console.log(sum)
+  //     const cartItem = {
+  //       itemId,
+  //       quantity
+  //     }
 
-//     setCartList([
-//       ...cartList.slice(0, idx),
-//       cartItem,
-//       ...cartList.slice(idx + 1)
-//     ])
-//   }
-// const handleQuantityChange = (quantity, itemId) => {
-//   onQuantityChange(itemId, quantity)
-// }
+  //     setCartList([
+  //       ...cartList.slice(0, idx),
+  //       cartItem,
+  //       ...cartList.slice(idx + 1)
+  //     ])
+  //   }
+  // const handleQuantityChange = (quantity, itemId) => {
+  //   onQuantityChange(itemId, quantity)
+  // }
 
-// const getTotal = () => {
-//   let cartIdArr = cartList.map((el) => el.itemId)
-//   let total = {
-//     price: 0,
-//     quantity: 0,
-//   }
-//   for (let i = 0; i < cartIdArr.length; i++) {
-//     if (checkedItems.indexOf(cartIdArr[i]) > -1) {
-//       let quantity = cartList[i]
-//       let price = cartList.filter((el) => el.id === cartList[i].itemId)[0].price
+  // const getTotal = () => {
+  //   let cartIdArr = cartList.map((el) => el.itemId)
+  //   let total = {
+  //     price: 0,
+  //     quantity: 0,
+  //   }
+  //   for (let i = 0; i < cartIdArr.length; i++) {
+  //     if (checkedItems.indexOf(cartIdArr[i]) > -1) {
+  //       let quantity = cartList[i]
+  //       let price = cartList.filter((el) => el.id === cartList[i].itemId)[0].price
 
-//       total.price = total.price + quantity * price
-//       total.quantity = total.quantity + quantity
-//     }
-//   }
-//   return total
-// }
+  //       total.price = total.price + quantity * price
+  //       total.quantity = total.quantity + quantity
+  //     }
+  //   }
+  //   return total
+  // }
 
-// const sumtotal = getTotal()
+  // const sumtotal = getTotal()
 
-const removeFromCart = (itemId) => {
-  setCartList(cartList.filter((el) => el.itemId !== itemId))
-}
+  const removeFromCart = (itemId) => {
+    setCartList(cartList.filter((el) => el.itemId !== itemId));
+  };
   return (
-  <div className="cart-container">
-    {/* <span id="shopping-cart-select-all">
+    <div className="cart-container">
+      {/* <span id="shopping-cart-select-all">
           <input
             type="checkbox"
             checked={
@@ -107,13 +114,23 @@ const removeFromCart = (itemId) => {
           </input>
           <label >전체선택</label>
         </span> */}
-    {cartList.map((item,idx)=>{
-      const quantity = 1
-      // console.log(`thisiscartList`, cartList)
-      return <CartList total={total} quantity={quantity} checkedItems={checkedItems} handleLogin={handleLogin} item={item} key ={idx} />
-    })}
-    <OrderSummary userinfo={userinfo} total={total} />
-    </div>)
+      {cartList.map((item, idx) => {
+        const quantity = 1;
+        // console.log(`thisiscartList`, cartList)
+        return (
+          <CartList
+            total={total}
+            quantity={quantity}
+            checkedItems={checkedItems}
+            handleLogin={handleLogin}
+            item={item}
+            key={idx}
+          />
+        );
+      })}
+      <OrderSummary userinfo={userinfo} total={total} />
+    </div>
+  );
 }
 
 export default Cart;
